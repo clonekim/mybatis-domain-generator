a1faea848eb3e2f77a1139bf3eb3829f844af5a4
daehee.kim <clonekim@gmail.com>
2019-07-05 13:17:38 +0900

    snakebar

--- src/main/kotlin/krud/template/CustomHelper.kt
+++ src/main/kotlin/krud/template/CustomHelper.kt
@@ -6,7 +6,7 @@ import com.github.jknack.handlebars.Options
 import org.apache.commons.text.CaseUtils
 import java.io.IOException
 
-enum class CustomHelper : Helper<Object> {
+enum class CustomHelper : Helper<Any> {
 
    firstCap {
        override fun toApply(value: Any, options: Options): String {
@@ -26,12 +26,22 @@ enum class CustomHelper : Helper<Object> {
         override fun toApply(value: Any, options: Options): String {
             return String.format("#{%s}", value.toString().toLowerCase())
         }
+    },
+
+    snakebar {
+        override fun toApply(value: Any, options: Options): String {
+            val regex = "([a-z])([A-Z]+)"
+            val replacement = "$1-$2"
+            return value.toString()
+                    .replace(regex.toRegex(), replacement)
+                    .toLowerCase()
+        }
     };
 
     protected  abstract fun toApply(value: Any, options: Options): String
 
     @Throws(IOException::class)
-    override fun apply(value: Object?, options: Options): Any? {
+    override fun apply(value: Any?, options: Options): Any? {
         return if(options.isFalsy(value)) {
             val param = options.param(0, null as Any?)
             param?.toString()
